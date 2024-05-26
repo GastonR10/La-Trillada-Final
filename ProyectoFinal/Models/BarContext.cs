036a95953c7e53ed2e56d4335e0b4a751ea3ea9d
Ignacio <Voory00712@gmail.com>
2024-05-25 21:18:17 -0300

    commit actualizacion 25/5

--- ProyectoFinal/Models/BarContext.cs
+++ ProyectoFinal/Models/BarContext.cs
@@ -1,13 +1,49 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ReglasNegocio.Entities;
 
 namespace ProyectoFinal.Models
 {
     public class BarContext : DbContext 
     {
-        //public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Admin> Administradores { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Mesa> Mesa { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<Carrito> Carritos { get; set; }
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<TipoProducto> TipoProductos { get; set; }
+        public DbSet<Reserva> Reservas { get; set; }
 
         public BarContext(DbContextOptions<BarContext> options): base(options) { 
             
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configurar la herencia de Pedido
+            modelBuilder.Entity<Pedido>()
+                .HasDiscriminator<string>("TipoPedido")
+                .HasValue<PedidoCliente>("Cliente")
+                .HasValue<PedidoExpress>("Express");
+
+            //usar datanotations en las clases.
+            // Configurar la clave primaria de Usuario
+            modelBuilder.Entity<Usuario>()
+                .HasKey(p => p.Id);
+            // Configurar la clave primaria de Carrito
+            modelBuilder.Entity<Carrito>()
+                .HasKey(p => p.Id);
+            // Configurar la clave primaria de Producto
+            modelBuilder.Entity<Producto>()
+                .HasKey(p => p.Id);
+            // Configurar la clave primaria de Reserva
+            modelBuilder.Entity<Reserva>()
+                .HasKey(p => p.Id);
+            // Configurar la clave primaria de TipoProducto
+            modelBuilder.Entity<TipoProducto>()
+                .HasKey(p => p.Id);
+
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 
--- ReglasNegocio/Entities/Pedido.cs
+++ ReglasNegocio/Entities/Pedido.cs
@@ -1,32 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ReglasNegocio.Entities
 {
-    public class Pedido
+    public abstract class Pedido
     {
-     
+
+        [Key]
         public int Id { get; set; }
-        public string Comentario { get; set; }
-        public string Estado { get; set; }
+
+        public string? Comentario { get; set; }
+
+        public string Estado { get; set; }// hacer enum para estado.
+
         public bool Aceptado { get; set; }
+
+        [ForeignKey(nameof(Carrito))]//definir FK
         public int IdCarrito { get; set; }
+
         public Carrito Carrito { get; set; }
+
+        [ForeignKey(nameof(Mesa))]
         public int IdMesa { get; set; }
 
         public DateTime Fecha { get; set; }
 
-        public Pedido(string comentario, string estado, bool aceptado, int idCarrito, int idMesa, DateTime fecha)
+        private Pedido() { //cambiar a public si me explota entity framework
+            Id = 0;
+            Comentario = null;
+            //Estado = Enums.Estados.Pendiente;
+            Aceptado = false;
+            IdCarrito = 0;
+            Carrito = null!;
+            IdMesa = 0;
+            Fecha = DateTime.Now;
+        }
+        
+        public Pedido(string? comentario, Carrito carrito, int idMesa): this() 
         {            
             Comentario = comentario;
-            Estado = estado;
-            Aceptado = aceptado;
-            IdCarrito = idCarrito;
+            Carrito = carrito;
             IdMesa = idMesa;
-            Fecha = DateTime.Now;  
         }
 
         
