e6be3a7f9c6d27c0dd6f13b3f1ddba9eeeffc313
Ignacio <Voory00712@gmail.com>
2024-05-26 19:57:06 -0300

    commitincial

--- ReglasNegocio/obj/Debug/net8.0/ReglasNegocio.AssemblyInfo.cs
+++ ReglasNegocio/obj/Debug/net8.0/ReglasNegocio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReglasNegocio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cf2ad2c82a23660dda78098477845dbd14687cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReglasNegocio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReglasNegocio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
