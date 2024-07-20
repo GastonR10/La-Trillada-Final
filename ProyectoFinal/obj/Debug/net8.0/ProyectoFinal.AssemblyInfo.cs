f5a9c133231961ba51fc9a8bde9d1fff9b227a6a
Gaston Roberto <GR161509@fi365.ort.edu.uy>
2024-07-20 18:12:08 -0300

    ddl

--- ProyectoFinal/obj/Debug/net8.0/ProyectoFinal.AssemblyInfo.cs
+++ ProyectoFinal/obj/Debug/net8.0/ProyectoFinal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoFinal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+853564385866ee2f9145b437d6eba6e3cd96a0c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0ad7b902c562e9eeeaca1020d939631e8e918b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoFinal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoFinal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
