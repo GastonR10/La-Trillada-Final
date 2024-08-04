bcb90ea482241c6ffa1da0704ee2bad15a6e3ac8
Gaston Roberto <GR161509@fi365.ort.edu.uy>
2024-08-04 12:51:03 -0300

    ddl

--- ProyectoFinal/obj/Debug/net8.0/ProyectoFinal.AssemblyInfo.cs
+++ ProyectoFinal/obj/Debug/net8.0/ProyectoFinal.AssemblyInfo.cs
@@ -11,10 +11,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("1acb8874-0189-4dda-9652-501382cfde69")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoFinal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0ad7b902c562e9eeeaca1020d939631e8e918b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82a53fef55b211566877c3f89b700cf5c7519c0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoFinal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoFinal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ReglasNegocio/obj/Debug/net8.0/ReglasNegocio.AssemblyInfo.cs
+++ ReglasNegocio/obj/Debug/net8.0/ReglasNegocio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReglasNegocio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0ad7b902c562e9eeeaca1020d939631e8e918b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82a53fef55b211566877c3f89b700cf5c7519c0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReglasNegocio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReglasNegocio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
